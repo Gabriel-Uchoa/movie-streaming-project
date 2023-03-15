18e7979ee8fe24e39ab9711798d39d99cbde8633
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2023-03-14 22:29:03 -0300

    correção no loading dos favoritos

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -2,9 +2,17 @@ import Footer from "../organisms/Footer"
 import NavBar from "../organisms/NavBar"
 import AllMoviesPage from "../organisms/AllMoviesPage"
 import usePopularMovies from "../../hooks/usePopularMovies"
+import { iDataUser } from "../../types/store.interface"
+import { useSelector } from "react-redux"
+import useFavoritesMovies from "../../hooks/useFavoritesMovies"
 
 const HomeTemplate: React.FC = () => {
+    const user_data: iDataUser = useSelector((state: any) => state.user_info);
+    const { movies_favorites } = useSelector((state: any) => state.movies);
+
     usePopularMovies()
+    useFavoritesMovies(user_data.personal_info.id, movies_favorites)
+
     return (
         <>
             <NavBar />
